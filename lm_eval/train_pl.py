ce14b5cd5c91da4acd445c020de655546edd3d3c
Yuval Kirstain <kirstain.yuval@gmail.com>
2021-06-11 12:13:27 +0300

    adding drop to evaluation

--- lm_eval/train_pl.py
+++ lm_eval/train_pl.py
@@ -82,7 +82,7 @@ def load_raw_datasests(train_set, save_prefix):
 def get_monitor_name(task_name):
     if task_name in ["arc_easy", "copa", "openbookqa", "lambada_cloze", "triviaqa", "piqa", "webqs", "nq_open", "winogrande", "race"]:
         return VAL_LOSS, "min"
-    elif task_name in ["rte", "sst", "wic", "multirc", "anli_r1", "wsc", "boolq", "squad2", "squad1"]:
+    elif task_name in ["rte", "sst", "wic", "multirc", "anli_r1", "wsc", "boolq", "squad2", "squad1", "drop"]:
         return VAL_ACC, "max"
     else:
         raise ValueError(f"We don't support task {task_name}")
